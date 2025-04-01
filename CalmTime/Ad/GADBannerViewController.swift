5ff2291f16c491fdd0435aab5e8aea234042f72d
Güneş Akgün <gunesakgun9@gmail.com>
2025-04-01 17:19:48 +0300

    add google admob banner

---
+++ CalmTime/Ad/GADBannerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GADBannerViewController.swift
+//  CalmTime
+//
+//  Created by Gunes Akgun on 1.04.2025.
+//
+
+import SwiftUI
+
+struct GADBannerViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GADBannerViewController()
+}
